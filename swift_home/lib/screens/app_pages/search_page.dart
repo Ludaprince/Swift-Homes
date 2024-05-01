c887c0524f73a901209dd3695b5910dfbda2478e
Amuzu Daniel <Danielamuzu05@gmail.com>
2024-05-01 23:15:55 +0000

    update the search UI

--- swift_home/lib/screens/app_pages/search_page.dart
+++ swift_home/lib/screens/app_pages/search_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter/widgets.dart';
+
 
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
@@ -9,7 +9,7 @@ class SearchPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.amber,
+        // backgroundColor: Colors.amber,
         appBar: _buildAppbar(context),
         body: Container(
           width: double.maxFinite,
@@ -25,7 +25,7 @@ class SearchPage extends StatelessWidget {
                   isDense: true,
                   filled: true,
                   fillColor: const Color.fromRGBO(245, 244, 248, 100),
-                  hintText: "Minix",
+                  hintText: "Search",
                   hintStyle: const TextStyle(
                     fontSize: 15,
                     fontWeight: FontWeight.normal,
@@ -49,6 +49,59 @@ class SearchPage extends StatelessWidget {
   }
 
   PreferredSizeWidget _buildAppbar(BuildContext context) {
-    return AppBar();
+    return CustomAppbar(
+      leadingWidth: 74,
+      leading: Container(
+        margin: const EdgeInsets.only(left: 24, top: 3, bottom: 3),
+        width: 50,
+        height: 50,
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: const Icon(Icons.arrow_back),
+      ),
+      centerTitle: true,
+      title: const Text("Search"),
+      // actions: [
+        
+      // ],
+    );
   }
+}
+
+class CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
+  final double? height;
+  final double? leadingWidth;
+  final Widget? leading;
+  final Widget? title;
+  final bool? centerTitle;
+  final double? actions;
+
+  const CustomAppbar(
+      {super.key,
+      this.height,
+      this.leadingWidth,
+      this.leading,
+      this.title,
+      this.centerTitle,
+      this.actions});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      toolbarHeight: height ?? 56,
+      automaticallyImplyLeading: false,
+      backgroundColor: Colors.transparent,
+      leadingWidth: leadingWidth ?? 0,
+      leading: leading,
+      title: title,
+      titleSpacing: 0,
+      centerTitle: centerTitle ?? false,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
